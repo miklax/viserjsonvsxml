397d9058673d041dff6c9c9f3d909c8910c20588
mikla <mikla79@gmail.com>
2015-12-29 22:05:06 +0100

    ctrl upd v20

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -3,16 +3,25 @@ angular.module('mainCtrl', ['getDataService'])
 .controller('merenjaController', function(Merenja){
   var vm = this;
 
-  vm.jsonTabela = [];
+  vm.jsonTabela = [
+    {velicina: 0, odziv: 0},
+    {velicina: 0, odziv: 0},
+    {velicina: 0, odziv: 0},
+    {velicina: 0, odziv: 0},
+  ];
   vm.xmlTabela = [];
+
+  vm.jsonPodaci = [];
+  vm.xmlPodaci = [];
 
   vm.getJsonMerenja = function(brojObj){
 
     Merenja.getJson(brojObj)
     .success(function(data, status, headers, config){
-        vm.jsonTabela = data;
-        console.log(vm.jsonTabela);
+        vm.jsonPodaci = data;
+        console.log(vm.jsonPodaci);
         console.log(headers('Velicina'));
+        vm.jsonTabela[0].velicina = headers('Velicina');
     });
   };
 });
