4500d5ad50c7578b3736474e7510d3e98572349d
mikla <mikla79@gmail.com>
2015-12-30 00:42:20 +0100

    tabela

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -3,16 +3,22 @@ angular.module('mainCtrl', ['getDataService'])
 .controller('merenjaController', function(Merenja){
   var vm = this;
 
-  vm.jsonTabela = [
-    {velicina: 0, odziv: 0},
-    {velicina: 0, odziv: 0},
-    {velicina: 0, odziv: 0},
-    {velicina: 0, odziv: 0},
-  ];
+  vm.jsonTabela = [];
   vm.xmlTabela = [];
 
   vm.jsonPodaci = [];
   vm.xmlPodaci = [];
+
+  function objMerenja(){
+    this.odziv = 0;
+    this.velicina = 0;
+  }
+
+  vm.jsonTabela[0] = new objMerenja(); //2000 obj
+  vm.jsonTabela[1] = new objMerenja(); //4000 obj
+  vm.jsonTabela[2] = new objMerenja(); //6000 obj
+  vm.jsonTabela[3] = new objMerenja(); //8000 obj
+  vm.jsonTabela[4] = new objMerenja(); //10000 obj
 
   vm.getJsonMerenja = function(brojObj){
 
